9eaac63fc045e63c39c6a3e3f31e24a1ce54b99b
zxam2 <minisoccer1653@naver.com>
2025-08-05 20:52:28 +0900

    Add files via upload

---
+++ 0805.py
@@ -0,0 +1,57 @@
+
+'''
+print("hello")
+print("world")
+
+print("hello", end="!")
+print("world")
+'''''
+'''
+a=input()
+b=input()
+a=int(a)
+b=int(b)
+print("%d+%d=%d"%(a,b,a+b))
+'''
+'''
+pi=3.141592
+
+print("원주율은 %f 이다"%pi)
+
+print("원주율은 %.2f 이다"%pi)
+'''
+'''
+a=int(input())
+b=int(input())
+
+
+print("%d+%d=%d"%(a, b, a+b))
+print("%d-%d=%d"%(a, b, a-b))
+print("%d*%d=%d"%(a, b, a*b))
+print("%d/%d=%.2f"%(a, b, a/b))
+print("%d//%d=%d"%(a, b, a//b))
+print("%d%% %d=%d"%(a, b, a%b))
+print("%d**%d=%d"%(a, b, a**b))
+'''
+
+
+
+
+
+
+
+'''
+name=input("이름: ")
+month=input("생일(월):")
+day=input("생일(일):")
+print("내 이름은 %s이고, 생일은 %s월 %s일 입니다."%(name, month, day))
+
+'''
+
+gold=500
+print("보유 골드:%d"%gold)
+item=int(input("아이템 가격:"))
+
+gold=gold-item
+
+print("남은 골드:%d"%gold)
