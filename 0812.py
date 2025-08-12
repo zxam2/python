b837e800d12ecb62a10d743d64396936b5200ed8
zxam2 <minisoccer1653@naver.com>
2025-08-12 20:54:13 +0900

    Add files via upload

---
+++ 0812.py
@@ -0,0 +1,101 @@
+'''a=[1,2,'a','b',3,4,'c']
+print(a[5])
+print(a[0:2])
+print(a[4:-1])
+print(a[0]+a[1])
+print(a[2]+a[3]+a[6])
+   '''
+'''a='I Love You'
+print(a.count('o'))
+print(a.find('y'),a.index('Y'))
+print('.'.join(a))
+print(a.split())
+print(a.replace('Love', 'Like'))
+print(a.upper(), a.lower())
+ '''   
+'''a=' abcd '
+b='efgh'
+print('%s%s' %(a,b))
+''''''
+a=[1, 5, 10, 3]
+a.append(20)
+print(a)
+a.sort()
+print(a)
+a=['b','a','n','a','n','a']
+a.reverse()
+print(a)
+a.remove('a')
+print(a)
+p=a.pop()
+print(p,a)
+n=a.count('n')
+print(n)
+'''
+
+
+'''
+a=[]
+
+a.append(input('1;'))
+
+a.append(input('2;'))
+
+a.append(input('3:'))
+print(a)'''
+'''
+a=input('3개 입력:').split()
+
+
+
+
+
+print(a)'''
+'''
+a=['c','e','a','b','d']
+
+a.sort()
+
+a.pop()
+print(a)
+
+'''
+'''
+a=input()
+
+b=[1,2,3,4,5,6]
+
+print(len(b))
+print(len(a))
+'''
+'''
+a=int(input())
+b=int(input())
+
+print("%d+%d=%d" %(a,b,a+b))
+
+print("%d-%d=%d" %(a,b,a-b))
+
+print("%d*%d=%d" %(a,b,a*b))
+
+print("%d/%d=%d.2f" %(a,b,a/b))
+
+print("%d//%d=%d" %(a,b,a//b))
+
+print("%d %% %d=%d" %(a,b,a%b))
+
+'''
+
+x=1
+y=0
+
+print('x:%s,y:%s' %(x,y))
+print('x and y=', x and y)
+print('x or y=', x or y)
+print('not x=', not x)
+print('not y=', not y)
+
+
+
+
+
